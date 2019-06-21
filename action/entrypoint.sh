206bb8ea6db32acefa8b493e4d63b867656e77db
Code Hz <codehz@users.noreply.github.com>
2019-06-21 22:43:25 +0800

    Update entrypoint.sh

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 git submodule update --init --recursive
-mkdir build
+mkdir -p build install
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=../install "$@" .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
 make
